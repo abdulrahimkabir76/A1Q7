63c4ad1c15cef374bf60ae3dfdaac523e85785da
abdulrahimkabir76 <abdulrahimkabir@gmail.com>
2026-02-16 19:38:50 +0500

    basic main structure

---
+++ main.py
@@ -0,0 +1,16 @@
+# main.py
+import pygame
+import sys
+import time
+
+from grid import Grid
+from node import Node
+
+def main():
+    grid = Grid()
+    running = True
+
+
+
+
+if __name__ == "__main__":    main()
